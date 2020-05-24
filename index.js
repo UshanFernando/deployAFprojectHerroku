9245169d9873c799e141c952277f458164f64bcd
ushanfernando <ushansankalpafernando@gmail.com>
2020-05-24 17:34:50 +0530

    jj

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 const mongouri = process.env.ATLAS_URI;
 
-app.use(express.static(path.join(__dirname, '/client/build')));
+app.use('*', express.static(path.join(__dirname, "client", "build")))
 
 app.use(cors());
 app.use(function (req, res, next) {
