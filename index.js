6881a4a41304c7656e125f9c6006a09c80625572
ushanfernando <ushansankalpafernando@gmail.com>
2020-05-24 17:20:46 +0530

    xx

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ app.use(function (req, res, next) {
   next();
 });
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 mongoose
   .connect(mongouri, {
     useNewUrlParser: true,
@@ -68,6 +70,7 @@ app.use((req, res, next) => {
   Er.status = 404;
   next(Er);
 });
+
 
 // app.use(function (erro, rq, res, next) {
 //   res.status(422).send({ error: erro.message });
